e8aa28311d9771356dbbd353b753694b78e46278
Lucian <montreal.lucian@gmail.com>
2017-03-20 15:33:40 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry : [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './redux-lesson-4/index.jsx'
+    './tea-router/index.jsx'
   ],
   output : {
     path : 'app/bundle',
